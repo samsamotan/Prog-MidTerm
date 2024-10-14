73b821f3f27d141eaf5c7e850055050360999c0f
samsamotan <samsamotan@gmail.com>
2024-10-14 19:59:27 +0800

    changes to menu screen

--- scenes/menu_scene.py
+++ scenes/menu_scene.py
@@ -2,7 +2,9 @@ import interactable_objects
 import scene
 
 menu_scene_objects = [
-    interactable_objects.InteractableObject("assets/new_game.png", 771, 161, 180, 360, False)
+    interactable_objects.InteractableObject("assets/new_game.png", 270, 48, 225, 150, False),
+    interactable_objects.InteractableObject("assets/load_game.png", 270, 48, 225, 210, False),
+    interactable_objects.InteractableObject("assets/save_game.png", 270, 48, 225, 270, False)
 ]
 
 menu_scene = scene.Scene('maps/black.jpg', menu_scene_objects)
