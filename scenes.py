32739acabee17605f0b53e7bcf1605098946dcf7
samsamotan <samsamotan@gmail.com>
2024-10-22 11:34:53 +0800

    minor changes to first scene

--- scenes.py
+++ scenes.py
@@ -6,16 +6,15 @@ from game_components.pacman import pacman_scene
 import pygame as pg
 
 first_scene_objects = [
-    scene_object.SceneObject("game_components/assets/tree.webp", 238, 280, 100, 100, True),
-    scene_object.SceneObject("game_components/assets/tree.webp", 238, 280, 500, 500, True),
-    interactable_objects.InteractableObject("game_components/assets/pixil-frame-0.png", 50, 30, 690, 220, False, interactions = {pg.K_e: "pacman"})
+    scene_object.SceneObject("game_components/assets/download.png", 800, 800, 500, 300, True),
+    interactable_objects.InteractableObject("game_components/assets/pixil-frame-0.png", 50, 30, 300, 220, False, interactions = {pg.K_e: "pacman"})
     ]
 first_scene = scene.Scene(False,'maps/black.jpg', first_scene_objects)
 
 menu_scene_objects = [
     interactable_objects.InteractableObject("game_components/assets/new_game.png", 270, 48, 225, 150, False, interactions = {pg.K_e: "first"}),
     interactable_objects.InteractableObject("game_components/assets/load_game.png", 270, 48, 225, 210, False, interactions = {pg.K_e: "first"}),
-    interactable_objects.InteractableObject("game_components/assets/save_game.png", 270, 48, 225, 270, False, interactions = {pg.K_e: "first"})
+    #interactable_objects.InteractableObject("game_components/assets/save_game.png", 270, 48, 225, 270, False, interactions = {pg.K_e: "first"})
 ]
 
 menu_scene = scene.Scene(False, 'maps/black.jpg', menu_scene_objects)
