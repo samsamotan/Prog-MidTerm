bbd11c9f32647f5048c1896c8cdcb81454697705
arianezcastro <castroariane563@gmail.com>
2024-11-01 19:16:02 +0800

    update

--- Tetris/grid.py
+++ Tetris/grid.py
@@ -1,4 +1,5 @@
 import pygame
+from color import Colors
 
 class Grid: 
     def __init__(self): 
@@ -8,7 +9,7 @@ class Grid:
 #the 2 dimensional array using for loop
         self.grid = [[0 for j in range(self.num_cols)] for i in range(self.num_rows)]
 #7 colors for the blocks; 1 color for the empty block
-        self.colors = self.get_cell_colors()
+        self.colors = Colors.get_cell_colors()
 
 #iterates over every cell in the grid and prints out its value row by row with each row printed on a new line
     def print_grid(self): 
@@ -17,21 +18,51 @@ class Grid:
                 print(self.grid[row][column], end = " ")
             print()
 
-#list of all the colors
-    def get_cell_colors(self):
+#to check if the block is inside the grid
+    def is_inside(self, row, column):
+        if row >= 0 and row < self.num_rows and column >= 0 and column < self.num_cols: 
+            return True
+        return False
+    
+    def is_empty(self, row, column):
+        if self.grid[row][column] == 0:
+            return True
+        return False
+    
+    #method to clear out full rows
+    #to check if row is full
+    def is_row_full(self,row):
+        for column in range(self.num_cols):
+            if self.grid[row][column] == 0:
+                return False
+        return True
 
-        dark_grey = (26, 31, 40)
-        green = (47, 230, 23)
-        red = (232, 18, 18)
-        orange = (226, 116, 17)
-        yellow = (237, 234, 4)
-        purple = (166, 0, 247)
-        cyan = (21, 204, 209)
-        blue = (13, 64, 216)
+    #to clear row
+    def clear_row(self, row):
+        for column in range(self.num_cols):
+            self.grid[row][column] = 0
+    
+    #to move incomplete rows down
+    def move_row_down(self, row, num_rows):
+        for column in range(self.num_cols):
+            self.grid[row+num_rows][column] = self.grid[row][column]
+            self.grid[row][column] = 0
+    
+    def clear_full_rows(self):
+        completed = 0
+        for row in range(self.num_rows-1, 0, -1):
+            if self.is_row_full(row):
+                self.clear_row(row)
+                completed += 1
+            elif completed > 0:
+                self.move_row_down(row, completed)
+        return completed
 
-        return [dark_grey, green, red, orange, yellow, purple, cyan, blue]
-
-
+    def reset(self):
+        for row in range(self.num_rows):
+            for column in range(self.num_cols):
+                self.grid[row][column] = 0
+            
     #drawing the cells of the grid with a specific color
     def draw(self, screen):
     #first, get the value stored in the cell using a nested for loop
@@ -41,7 +72,7 @@ class Grid:
                 cell_value = self.grid[row][column]
                 #rect to contain the cell 
                 #x coordinate,y coordinate (top left),width,height
-                cell_rect = pygame.Rect(column*self.cell_size +1, row*self.cell_size +1,
+                cell_rect = pygame.Rect(column*self.cell_size + 11, row*self.cell_size + 11,
                             self.cell_size -1, self.cell_size -1)
                  #3 arguments (surface=screen, color, rect)
                 pygame.draw.rect(screen, self.colors[cell_value], cell_rect)
--- Tetris/main.py
+++ Tetris/main.py
@@ -1,20 +1,32 @@
 import pygame, sys
-from grid import Grid
+from game import Game
+from color import Colors 
 
 #To initialize pygame
 pygame.init()
-dark_blue = (44, 44, 127)
+
+#text to display the score (font family, size)
+title_font = pygame.font.Font(None, 40)
+score_surface = title_font.render("SCORE", True, Colors.white)
+next_surface = title_font.render("NEXT", True, Colors.white)
+game_over_surface = title_font.render("GAME OVER!", True, Colors.white)
+
+score_rect = pygame.Rect(320, 55, 170, 60)
+next_rect = pygame.Rect(320, 215, 170, 180)
 
 #the display surface
 #set_mode((width, height))
-screen = pygame.display.set_mode((300, 600))
-pygame.display.set_caption("Packing Packets")
+screen = pygame.display.set_mode((500, 620))
+pygame.display.set_caption("Tetris")
 
 #controls the frame rate of the game; how fast the game will run
 clock = pygame.time.Clock()
 
-game_grid = Grid()
-game_grid.print_grid()
+game = Game()
+
+#make the count of the block going down slower
+GAME_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(GAME_UPDATE, 200)
 
 #the game loop-defining a way to stop the game's execution
 while True:
@@ -25,9 +37,40 @@ while True:
             pygame.quit()
             #command to exit the game
             sys.exit()
-    #the Canvas
-    screen.fill(dark_blue)
-    game_grid.draw(screen)
+        if event.type == pygame.KEYDOWN:
+            if game.game_over == True:
+                game.game_over = False
+                game.reset()
+            if event.key == pygame.K_LEFT and game.game_over == False:
+                game.move_left()
+            if event.key == pygame.K_RIGHT and game.game_over == False:
+                game.move_right()
+            if event.key == pygame.K_DOWN and game.game_over == False:
+                game.move_down()
+                game.update_score(0)
+            if event.key == pygame.K_UP and game.game_over == False:
+                game.rotate() 
+        if event.type == GAME_UPDATE and game.game_over == False:
+            game.move_down()
+
+    #Drawing/the Canvas
+    score_value_surface = title_font.render(str(game.score), True, Colors.white)
+
+    screen.fill(Colors.dark_blue)
+    screen.blit(score_surface, (358, 20, 50, 50))
+    screen.blit(next_surface, (368, 180, 50, 50))
+
+    if game.game_over == True: 
+        screen.blit(game_over_surface, (318, 450, 50, 50))
+
+    pygame.draw.rect(screen, Colors.light_blue, score_rect, 0, 10)
+    screen.blit(score_value_surface, 
+                score_value_surface.get_rect(centerx = score_rect.centerx,
+                                             centery = score_rect.centery))
+    pygame.draw.rect(screen, Colors.light_blue, next_rect, 0, 10)
+    game.draw(screen)
+    #for the block to move down on its own
+
     #take all the changes we made in the objects and draws a picture of them
     pygame.display.update()
     #tick tells the clock how fast the game should run (frames per second)
