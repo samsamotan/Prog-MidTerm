865d470347155d6158fbe7ece54c88f5c0890a88
samsamotan <samsamotan@gmail.com>
2024-11-04 09:01:36 +0800

    fixed start scene

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ scene_manager.add_scene("Packing Packets", PackingPackets)
 
 # TODO: move sprite group declarations for scenes to init(will cause phantoms if scene manager reset not fixed)
 
-scene_manager.start_scene("Virus Vacuum")
+scene_manager.start_scene("Main Menu")
 
 clock = pg.time.Clock()
 
