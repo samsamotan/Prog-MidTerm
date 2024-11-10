92391c964899e2bf441218ca5dad030c5dbaebb3
samsamotan <samsamotan@gmail.com>
2024-11-10 22:38:16 +0800

    meow

--- game_components/objects/NPC.py
+++ game_components/objects/NPC.py
@@ -5,14 +5,14 @@ from .chatbox import Chatbox
 
 # NPC Class
 class NPC(pygame.sprite.Sprite):
-    def __init__(self, x, y, messages):
+    def __init__(self, x, y, width, height, messages):
         super().__init__()
-        self.image = pygame.Surface((50, 50))  # Placeholder for the NPC image
-        self.image.fill((150, 0, 0))  # Red color as NPC placeholder
+        self.image = pygame.Surface((width, height))  # Placeholder for the NPC image
+        self.image.fill((0, 0, 0, 100))  # Red color as NPC placeholder
         self.rect = self.image.get_rect(topleft=(x, y))
         self.chatbox = Chatbox(messages)
         self.in_proximity = False
-        self.font = pygame.font.Font(None, 24)  # Font for the "E" prompt
+        self.font = pygame.font.Font(None, 32)  # Font for the "E" prompt
 
     def update(self, player_pos):
         # Check if player is in proximity and toggle the interaction prompt
@@ -27,7 +27,7 @@ class NPC(pygame.sprite.Sprite):
     def draw_prompt(self, surface, camera):
         # Draw interaction prompt (letter "E") above NPC if in proximity
         if self.in_proximity:
-            prompt_text = self.font.render("E", True, (160, 32, 240))  # Yellow color for "E"
+            prompt_text = self.font.render("E", True, (160, 32, 240))  # Purple color for "E"
             surface.blit(prompt_text, camera.apply((self.rect)).move(60,-10))  # Display above the NPC
 
     def draw_chatbox(self, surface):
--- game_components/scenes/main_scene.py
+++ game_components/scenes/main_scene.py
@@ -16,7 +16,7 @@ class MainScene(Scene):
         background = GameObject(0, 0, image = os.path.join(assets_folder, "map1.png"))
         print (background.rect)
         self.player = Player(980, 470, 15, 20, os.path.join(assets_folder, "cowboy.png"))
-        npc1 = NPC(830, 1180,
+        npc1 = NPC(830, 1180, 50, 50,
                     ["Ralof: Hey, you. You're finally awake. You were trying to cross the border, right? Walked right into that Imperial ambush, same as us, and that thief over there.", 
                      "Lokir: Damn you Stormcloaks. Skyrim was fine until you came along. Empire was nice and lazy. If they hadn't been looking for you, I could've stolen that horse and been half way to Hammerfell. You there. You and me -- we should be here. It's these Stormcloaks the Empire wants.", 
                      "Ralof: We're all brothers and sisters in binds now, thief.", "Imperial Soldier: Shut up back there!", 
@@ -28,6 +28,7 @@ class MainScene(Scene):
                      "Lokir: Why do you care?", 
                      "Ralof: A Nord's last thoughts should be of home.", 
                      "Lokir: Rorikstead. I'm...I'm from Rorikstead."])
+        cat = NPC(970, 380, 50, 50, ["Meow", "Meow", "Meow", "Meow", "Meow"])
         portal_to_vacuum = InteractiveObject(970, 1400, 50, 30, self.scene_manager, "Main Scene", "Virus Vacuum", os.path.join(assets_folder, "pixil-frame-0.png"))
         portal_to_vacuum.add_action(pygame.K_e, "change scene")
         portal_to_firewall = InteractiveObject(400, 300, 50, 30, self.scene_manager, "Main Scene", "Firewall Fighter", os.path.join(assets_folder, "pixil-frame-0.png"))
@@ -43,7 +44,7 @@ class MainScene(Scene):
         self.all_sprites.add(background, walls, npc1, portal_to_vacuum, portal_to_firewall, portal_to_password, portal_to_packets, portal_to_color)
         self.obstacles.add(walls)
         self.npc_group = pygame.sprite.Group()
-        self.npc_group.add(npc1)
+        self.npc_group.add(npc1, cat)
 
     def handle_events(self, dt):
         for interaction in self.interactions:
